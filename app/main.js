03b342dee5a9008fd21855af57aaa5e07618ed81
Carson Plant <carsonplant22@gmail.com>
2019-08-19 15:39:30 -0600

    deer

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
 import Alligator from "./models/Alligator.js";
 import Bear from "./models/Bear.js";
 import Camel from "./models/Camel.js";
+import Deer from "./models/Deer.js";
 
 let ally = new Alligator("Ally", "Green", "F", 200, 60, "big", "Adult");
 let bobo = new Bear("Bobo", "Grizzly", "M", "Standing: 8ft", 684, "Mature", "Montana");
 let carlos = new Camel("Carlos", 2, "F", "5ft", 942, "Adult", "Saudi Arabia");
+let donny = new Deer("Donny", "Mule", "F", 0, "4ft", 127, "Youth", "Colorado");
---
+++ app/models/Deer.js
@@ -0,0 +1,12 @@
+export default class Deer {
+  constructor(name, type, gender, antlers, height, weight, lifeStage, birthPlace) {
+    this.name = name;
+    this.type = type;
+    this.gender = gender;
+    this.antlers = antlers;
+    this.height = height;
+    this.weight = weight;
+    this.lifeStage = lifeStage;
+    this.birthPlace = birthPlace;
+  }
+}
