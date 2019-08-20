cb198d8ad255f56f44b5ca7ff4adfc83a484ed11
Carson Plant <carsonplant22@gmail.com>
2019-08-19 21:28:00 -0600

    through Panda

--- app/main.js
+++ app/main.js
@@ -11,20 +11,26 @@ import Jaguar from "./models/Jaguar.js";
 import Kangaroo from "./models/Kangaroo.js";
 import Lemur from "./models/Lemur.js";
 import Meerkat from "./models/Meerkat.js";
+import Nyala from "./models/Nyala.js";
+import Ostrich from "./models/Ostrich.js";
+import Panda from "./models/Panda.js";
 
 let ally = new Alligator("Ally", "Green", "F", 200, 60, "big", "Adult", "Florida");
 let bobo = new Bear("Bobo", "Grizzly", "Brown", "M", "Standing: 8ft", 684, "Mature", "Montana");
-let carlos = new Camel("Carlos", "Sandy", 2, "F", "5ft", 942, "Adult", "Saudi Arabia");
-let donny = new Deer("Donny", "Mule", "Brown", "F", 0, "4ft", 127, "Youth", "Colorado");
+let carly = new Camel("Carly", "Sandy", 2, "F", "5ft", 942, "Adult", "Saudi Arabia");
+let darla = new Deer("Darla", "Mule", "Brown", "F", 0, "4ft", 127, "Youth", "Colorado");
 let earnie = new Elephant("Earnie", "African Bush", "Grey", 2, true, "M", "11.5ft", 10753, "Mature", "Ethiopia");
-let frankie = new Flamingo("Frankie", "Pink", "F", "4ft", 6, "Adult", "Chile");
+let franny = new Flamingo("Franny", "Pink", "F", "4ft", 6, "Adult", "Chile");
 let giovani = new Giraffe("Giovani", "Northern Giraffe", "Sandy base with brown spots", "M", "11ft", 1020, "Youth", "Zambia");
 let hank = new Hippo("Hank", "Pinkish-Blueish-Grey", "M", "5ft", 4221, "Mature", "Uganda");
 let iggy = new Iguana("Iggy", "Blue Iguana", "Turqouise", true, true, "M", "1ft", 27, "5ft", "Adult", "Cayman Islands");
-let jerry = new Jaguar("Jerry", "Black", "F", true, "2ft", "175", "Adult", "Bolivia");
-let katy = new Kangaroo("Katy", "Eastern Grey", "Tan/Grey", "M", "5.5ft", 109, "Adult", "Australia");
+let jade = new Jaguar("Jade", "Black", "F", true, "2ft", "175", "Adult", "Bolivia");
+let karl = new Kangaroo("Karl", "Eastern Grey", "Tan/Grey", "M", "5.5ft", 109, "Adult", "Australia");
 let larry = new Lemur("Larry", "Ring-tailed", "Grey with black and white tail", true, "M", "1.5ft", 5.5, "Adult", "Madagascar");
-let mark = new Meerkat("Mark", "Tan/Sandy", "F", "0.75", 1, "Youth", "South Africa");
+let mary = new Meerkat("Mary", "Tan/Sandy", "F", "0.75", 1, "Youth", "South Africa");
+let nikko = new Nyala("Nikko", "Dark brown with subtle white stripes", "M", true, "3.5ft", 209, "Adult", "Zimbabwe");
+let oscar = new Ostrich("Oscar", "Black", "M", "9ft", 239, "Mature", "Kenya");
+let peter = new Panda("Peter", "Giant", "Black and white", "M", "Standing: 5.5ft", 199, "Adult", "China");
 
 
 
---
+++ app/models/Nyala.js
@@ -0,0 +1,12 @@
+export default class Nyala {
+  constructor(name, color, gender, antlers, height, weight, lifeStage, birthPlace) {
+    this.name = name;
+    this.color = color;
+    this.gender = gender;
+    this.antlers = antlers;
+    this.height = height;
+    this.weight = weight;
+    this.lifeStage = lifeStage;
+    this.birthPlace = birthPlace;
+  }
+}
